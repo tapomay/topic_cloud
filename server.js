1ecceb58cb8ebf00275e230d139006709b003f4a
tapomay <tapomay.dey@gmail.com>
2017-05-09 04:16:25 -0700

    Kafka initial integration

--- server.js
+++ server.js
@@ -2,20 +2,83 @@
 
 const express = require('express');
 const socketIO = require('socket.io');
-const path = require('path');
 
-const PORT = process.env.PORT || 3000;
+const kafka = require('kafka-node');
+const HighLevelConsumer = kafka.HighLevelConsumer;
+const Offset = kafka.Offset;
+const Client = kafka.Client;
+
+
+// PARSE ARGS
+var argv = require('yargs')
+           .boolean('from_beginning')
+           .boolean('verbose').argv;
+
+if(argv.help){
+  console.log("--zk=cs185:5181 --dataTopic data --metaTopic meta --port=8080");
+  process.exit(code=0)
+}
+
+var port = argv.port || 9080;
+var zk = argv.zk || 'cs185:5181';
+var topiclist = (argv.topic || 'weather_display').split(",");
+var fromBeginning = argv.from_beginning || true;
+var verbose = argv.verbose || true;
+var dataTopic = argv.dataTopic || 'data';
+var metaTopic = argv.metaTopic || 'meta';
+var topics = [{topic: dataTopic}, {topic: metaTopic}];
+
+// INIT KAFKA
+var client = new Client(zk);
+var options = { autoCommit: true, fromBeginning: fromBeginning, fetchMaxWaitMs: 1000, fetchMaxBytes: 1024*1024 };
+var consumer = new HighLevelConsumer(client, topics, options);
+var offset = new Offset(client);
+
+// INIT SERVER AND SOCKETIO
+const path = require('path');
 const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+  .listen(port, () => console.log(`Listening on ${ port }`));
 
-const io = socketIO(server);
+const io = require('socket.io')(server);
+
+// KAFKA CONSUMER
+consumer.on('message', function (message) {
+    if(verbose){
+      console.log(this.id, message);
+    }else{
+      //console.log(message.topic);
+    }
+    try{
+      var msg = message.value;
+      // try{
+      //   msg = JSON.parse(message.value);
+      // }catch(e){
+      //    //ok it's not json
+      // }
+      io.emit(message.topic, msg);
+    }catch(err){
+      console.log('error', err);
+    }
+});
+
+consumer.on('error', function (err) {
+    console.log('error', err);
+});
+
+consumer.on('offsetOutOfRange', function (topic) {
+    topic.maxNum = 2;
+    offset.fetch([topic], function (err, offsets) {
+        var min = Math.min.apply(null, offsets[topic.topic][topic.partition]);
+        consumer.setOffset(topic.topic, topic.partition, min);
+    });
+});
 
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+// setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
